a4d5ccfb86d4559e0f0985589681e2b91f57f2b8
victor1ahermn <victoriahermn@javerianacali.edu.co>
2022-05-20 22:56:53 -0500

    Se crea funcion recolectar_datos_detalle_criterio en Jurado_View-py con el fin de reutilizar codigo. Se elimina funcion en clase DetalleCriterio y se adiciona a clase Controlador con retorno de la nota final del criterio

--- controller/Controlador.py
+++ controller/Controlador.py
@@ -6,18 +6,24 @@ class Controlador:
         super().__init__()
         self.actas = {}
         self.current_length = 0
-        self.criterios = {1: Criterio(1, "Desarrollo y profundidad en el tratamiento del tema",
+        self.criterios = {1: Criterio("1", "Desarrollo y profundidad en el tratamiento del tema",
                                       "", 0.20),
-                          2: Criterio(2, "Desafío académico y científico del tema",
+                          2: Criterio("2", "Desafío académico y científico del tema",
                                       "", 0.15),
-                          3: Criterio(3, "Cumplimiento de los objetivos propuestos",
+                          3: Criterio("3", "Cumplimiento de los objetivos propuestos",
                                       "", 0.10),
-                          4: Criterio(4, "Creatividad e innovación de las soluciones y desarrollos propuestos:",
+                          4: Criterio("4", "Creatividad e innovación de las soluciones y desarrollos propuestos:",
                                       "", 0.10),
-                          5: Criterio(5, "Validez de los resultados y conclusiones",
+                          5: Criterio("5", "Validez de los resultados y conclusiones",
                                       "", 0.10),
-                          6: Criterio(6, "Manejo y procesamiento de la información y bibliografía",
+                          6: Criterio("6", "Manejo y procesamiento de la información y bibliografía",
                                       "", 0.10),
-                          7: Criterio(7, "Calidad y presentación del documento escrito",
+                          7: Criterio("7", "Calidad y presentación del documento escrito",
                                       "", 0.075),
-                          8: Criterio(8, "Presentación oral", "", 0.075)}
+                          8: Criterio("8", "Presentación oral", "", 0.075)}
+
+    def calcular_nota_criterio(self, calificacion1, calificacion2, identificador_criterio):
+        ponderado = self.criterios[int(identificador_criterio)].porcentaje_ponderacion
+        nota_criterio = ((float(calificacion1) + float(calificacion2)) / 2) * ponderado
+
+        return nota_criterio
